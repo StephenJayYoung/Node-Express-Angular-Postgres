4779253d9d56573681640d31c6a7e6d26f7505a1
Stephen Young <stephenjayyoung@gmail.com>
2015-06-22 13:54:01 -0700

    installed dependencies, added comments for working with database

--- node-postgres-todo/models/database.js
+++ node-postgres-todo/models/database.js
@@ -5,3 +5,8 @@ var client = new pg.Client(connectionString);
 client.connect();
 var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
 query.on('end', function() { client.end(); });
+
+// try to create a table with a few columns, insert data into it, 
+// retrieve the results from that table, drop the table (try to change this by using specific functions)
+//Read documentaion on pg, and then see if there is documentation 
+//on a postgres model based on an ORM (object relational model)
